56082354ffb250ff6122c7970451a25e6e6440b6
Hao-Ming, Hsu <50090692+minghsu0107@users.noreply.github.com>
2022-02-19 11:01:13 +0800

    Create string_template.go

---
+++ basics/string_template.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+  "fmt"
+  "html/template"
+  "strings"
+)
+
+func main() {
+  data := map[string]interface{}{
+    "Name":     "Bob",
+    "UserName": "bob92",
+    "Roles":    []string{"dbteam", "uiteam", "tester"},
+  }
+
+  t := template.Must(template.New("email").Parse(emailTmpl))
+  builder := &strings.Builder{}
+  if err := t.Execute(builder, data); err != nil {
+    panic(err)
+  }
+  s := builder.String()
+  fmt.Println(s)
+
+}
+
+const emailTmpl = `Hi {{.Name}}!
+
+Your account is ready, your user name is: {{.UserName}}
+
+You have the following roles assigned:
+{{range $i, $r := .Roles}}{{if $i}}, {{end}}{{.}}{{end}}
+`
