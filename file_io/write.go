ba625ef566518ab73eb2b526d5e718a28d2d4c78
Hao-Ming Hsu <50090692+minghsu0107@users.noreply.github.com>
2023-09-25 11:07:41 -0700

    Update write.go

--- file_io/write.go
+++ file_io/write.go
@@ -3,7 +3,6 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"io/ioutil"
 	"os"
 )
 
@@ -16,7 +15,7 @@ func check(e error) {
 func main() {
 
 	d1 := []byte("hello\ngo\n")
-	err := ioutil.WriteFile("/tmp/dat1", d1, 0644)
+	err := os.WriteFile("/tmp/dat1", d1, 0644)
 	check(err)
 
 	f, err := os.Create("/tmp/dat2") // f is *File (pointer)
